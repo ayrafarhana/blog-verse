1e11957cdc138c34760cc6d6db7f2bbc44567a12
ayrafarhana <shaik.farhana2024@gmail.com>
2025-09-06 12:36:15 +0530

    learnt something

--- first.js
+++ first.js
@@ -1,1 +1,32 @@
-console.log("Hello Miss Ayra") 
+console.log("Hello ") 
+let name = "Miss Ayra"
+let age = 20
+
+console.log(age)
+console.log(name)
+
+let islogged = false 
+console.log(islogged)
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof islogged)
+
+let x 
+console.log(typeof x)
+
+let y = null 
+console.log(typeof y)
+console.log(y)
+
+let arr = [1,2,3]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name : "ayra", 
+    age1 : 19 
+}
+
+console.log(person.name) 
+console.log(person.age1)       
